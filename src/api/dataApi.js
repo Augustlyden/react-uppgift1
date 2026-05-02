f8ca4a3cf1934786387d625d13766598a161e9aa
Augustlyden <august.lyden@outlook.com>
2026-05-02 17:04:11 +0200

    all api functions

--- src/api/dataApi.js
+++ src/api/dataApi.js
@@ -18,6 +18,33 @@ export const getSpellByIndex = async (index) => {
   }
 }
 
+export const createSpell = async (spellData) => {
+  try {
+    const respone = await apiClient.post('/spells', spellData);
+    return respone.data;
+  } catch (error) {
+    throw new Error(`createSpell failed: ${error.message}`);
+  }
+}
+
+export const updateSpell = async (index, spellData) => {
+  try {
+    const respone = await apiClient.patch(`/spells/${index}`, spellData);
+    return respone.data;
+  } catch (error) {
+    throw new Error(`updateSpell ${index} failed: ${error.message}`);
+  }
+}
+
+export const deleteSpell = async (index) => {
+  try {
+    const response = await apiClient.delete(`/spells/${index}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`deleteSpell ${index} failed: ${error.message}`);
+  }
+}
+
 export const getAllEquipment = async () => {
   try {
     const response = await apiClient.get('/equipment');
@@ -32,5 +59,32 @@ export const getEquipmentByIndex = async (index) => {
     const response = await apiClient.get(`/equipment/${index}`);
   } catch {
     throw new Error(`getEquipemtByIndex ${index} failed: ${error.message}`);
+  }
+}
+
+export const createEquipment = async (equipmentData) => {
+  try {
+    const respone = await apiClient.post('/equipment', equipmentData);
+    return respone.data;
+  } catch (error) {
+    throw new Error(`createSpell failed: ${error.message}`);
+  }
+}
+
+export const updateEquipment = async (index, equipmentData) => {
+  try {
+    const respone = await apiClient.patch(`/equipment/${index}`, equipmentData);
+    return respone.data;
+  } catch (error) {
+    throw new Error(`updateSpell ${index} failed: ${error.message}`);
+  }
+}
+
+export const deleteEquipment = async (index) => {
+  try {
+    const response = await apiClient.delete(`/equipment/${index}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`deleteSpell ${index} failed: ${error.message}`);
   }
 }
